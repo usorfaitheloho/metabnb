5a5b79bbea2d2527fa7a31cccb911924933e3af7
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-12 21:35:33 -0800

    Add nav data

---
+++ src/component/Header/NavItems.js
@@ -0,0 +1,25 @@
+const Data = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '/',
+    },
+    {
+        id: 2,
+        name: 'Place to stay',
+        href: '/place-to-stay',
+    },
+    {
+        id: 3,
+        name: 'NFTS',
+        href: '/nfts',
+    },
+    {
+        id: 4,
+        name: 'Community',
+        href: '/community',
+    },
+
+];
+
+export default Data;
