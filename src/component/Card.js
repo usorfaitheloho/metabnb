31b1399c18008a883530ce41277959eb596fe2c3
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-14 19:56:00 -0800

    Add Card component and logic

---
+++ src/component/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = ({item}) => {
+  return (
+    <div className="w-fit p-3 border border-[#d7d7d7] rounded-[15px] cursor-pointer">
+    <img className="h-[120px] lg:[220px] mb-4" src={item.img} alt="Inspirations-image" />
+    <div className="flex flex-col lg:flex-row text-[8px] lg:text-[10px]">
+        <p className="mb-2"> {item.name}</p>
+        <p className="font-bold">{item.price}</p>
+    </div>
+    <div className="flex flex-col lg:flex-row text-[8px] lg:text-[1px] justify-between my-2">
+        <p className="mb-2">{item.distance}</p>
+        <p>{item.availability}</p>
+    </div>
+    <span>
+        <img src="{item.rating}" alt="Rating" className="h-[10px] lg:h-[15px]" />
+    </span>
+</div>
+  )
+}
+
+export default Card;
