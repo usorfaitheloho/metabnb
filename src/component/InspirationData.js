5d6cbb0f5b5c02648bf4d453b14f463a4d54910d
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-15 05:05:19 -0800

    Add images to inspiration Data

--- src/component/InspirationData.js
+++ src/component/InspirationData.js
@@ -1,3 +1,14 @@
+import Insp1 from "../images/Insp1.png";
+import Insp2 from "../images/Insp2.png";
+import Insp3 from "../images/Insp3.png";
+import Insp4 from "../images/Insp4.png";
+import Insp5 from "../images/Insp5.png";
+import Insp6 from "../images/Insp6.png";
+import Insp7 from "../images/Insp7.png";
+import Insp8 from "../images/Insp8.png";
+import StarRating from "../images/StarRating.png";
+
+
 export const InspirationData = [
     {
         img: Insp1,
@@ -57,7 +68,7 @@ export const InspirationData = [
 		rating: StarRating,
 	},
     {
-		img: InspImg8,
+		img: Insp8,
 		name: "Desert king",
 		price: "1MBT per night",
 		distance: "2345km away",
