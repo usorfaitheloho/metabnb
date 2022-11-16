d57f27f74bab681f8d96560780ed644cc4db63bb
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-15 22:42:08 -0800

    Add inspiration Data

---
+++ src/data/InspirationData.js
@@ -0,0 +1,78 @@
+import Insp1 from "../images/Insp1.png";
+import Insp2 from "../images/Insp2.png";
+import Insp3 from "../images/Insp3.png";
+import Insp4 from "../images/Insp4.png";
+import Insp5 from "../images/Insp5.png";
+import Insp6 from "../images/Insp6.png";
+import Insp7 from "../images/Insp7.png";
+import Insp8 from "../images/Insp8.png";
+import StarRating from "../images/StarRating.png";
+
+
+export const InspirationData = [
+    {
+        img: Insp1,
+        name: "Desert king" ,
+        price: "1MBT per night" ,
+        distance: "2345km away",
+        availability: "available for 2 weeks stay",
+        rating: StarRating,
+
+    },
+    {
+		img: Insp2,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: Insp3,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: Insp4,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: Insp5,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: Insp6,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: Insp7,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: Insp8,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+];
