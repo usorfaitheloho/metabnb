bc62e9efb6b7b05b9b2a0013edb35919d34fdc99
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-15 22:37:54 -0800

    Fix link in InspirationData

--- src/component/Inspiration.js
+++ src/component/Inspiration.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "./Card";
-import { InspirationData } from "./InspirationData";
+import { InspirationData } from "../data/InspirationData";
 
 const Inspiration = () => {
 	return (
