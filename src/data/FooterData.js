1d3be8b1c9397075b07814050d99c5d26077ecbd
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-15 22:41:48 -0800

    Add Footer Data

---
+++ src/data/FooterData.js
@@ -0,0 +1,84 @@
+import TwitterLogo from "../images/TwitterLogo.png";
+import FacebookLogo from "../images/FacebookLogo.png";
+import InstagramLogo from "../images/InstagramLogo.png";
+
+export const FooterData = [
+	{
+		title: "Community",
+		sublinks: [
+			{
+				name: "NFT",
+				link: "/",
+			},
+			{
+				name: "Tokens",
+				link: "/",
+			},
+			{
+				name: "Landlords",
+				link: "/",
+			},
+			{
+				name: "Discord",
+				link: "/",
+			},
+		],
+	},
+	{
+		title: "Places",
+		sublinks: [
+			{
+				name: "Castle",
+				link: "/",
+			},
+			{
+				name: "Farms",
+				link: "/",
+			},
+			{
+				name: "Beach",
+				link: "/",
+			},
+			{
+				name: "Learn more",
+				link: "/placeToStay",
+			},
+		],
+	},
+	{
+		title: "About us",
+		sublinks: [
+			{
+				name: "Road map",
+				link: "/",
+			},
+			{
+				name: "Creators",
+				link: "/",
+			},
+			{
+				name: "Career",
+				link: "/",
+			},
+			{
+				name: "Contact us",
+				link: "/",
+			},
+		],
+	},
+];
+
+export const FooterSocials = [
+	{
+		name: FacebookLogo,
+		link: "/",
+	},
+	{
+		name: InstagramLogo,
+		link: "/",
+	},
+	{
+		name: TwitterLogo,
+		link: "/",
+	},
+];
