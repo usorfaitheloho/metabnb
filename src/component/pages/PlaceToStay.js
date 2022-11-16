235e82f1f19ba2a92703535e54a2e2b8917dee50
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-15 23:33:19 -0800

    Add redirect and Fix hamburger

--- src/component/pages/PlaceToStay.js
+++ src/component/pages/PlaceToStay.js
@@ -19,8 +19,8 @@ const PlaceToStay = () => {
    
     <Navbar handleModal={handleModal} />
     <div className="py-8 lg:py-[3rem] px-2 lg:px-8 max-w-[1200px] mx-auto">
-        <div className="flex flex-col lg:flex-row items-center text-sm lg:w-[90%] mx-auto mb-[2rem]">
-                <div className="flex justify-between flex-nowrap w-full lg:scrollbar-hide scroll-smooth mb-4 lg:mb-0 py-4">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center text-sm lg:w-[90%] mx-auto mb-[2rem]">
+                <div className="flex flex-col lg:flex-row justify-center lg:justify-between text-center gap-y-4 flex-nowrap w-full lg:scrollbar-hide scroll-smooth mb-4 lg:mb-0 py-4">
                     {PlaceToStayFilters.map((item,id) =>(
                         <Link key={id} to={item.link} className="text-xs whitespace-nowrap mx-4">{item.name}</Link>
                     ))}
