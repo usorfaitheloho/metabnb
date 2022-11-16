863931d7db92484b3d5e2c34c6e3831a470ce95f
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-15 18:00:13 -0800

    Add logic in PlaceTo Stay component

---
+++ src/data/PlaceToStayData.js
@@ -0,0 +1,186 @@
+import LocationImg1 from '../images/LocationImg1.png';
+import LocationImg2 from '../images/LocationImg2.png';
+import LocationImg3 from '../images/LocationImg3.png';
+import LocationImg4 from '../images/LocationImg4.png';
+import LocationImg5 from '../images/LocationImg5.png';
+import LocationImg6 from '../images/LocationImg6.png';
+import LocationImg7 from '../images/LocationImg7.png';
+import LocationImg8 from '../images/LocationImg8.png';
+import LocationImg9 from '../images/LocationImg9.png';
+import LocationImg11 from '../images/LocationImg11.png';
+import LocationImg12 from '../images/LocationImg12.png';
+import LocationImg13 from '../images/LocationImg13.png';
+import LocationImg14 from '../images/LocationImg14.png';
+import LocationImg15 from '../images/LocationImg15.png';
+import LocationImg16 from '../images/LocationImg16.png';
+import LocationImg17 from '../images/LocationImg17.png';
+import StarRating from "../images/StarRating.png";
+
+export const PlaceToStayData = [
+    {
+        img: LocationImg1,
+        name: "Desert king" ,
+        price: "1MBT per night" ,
+        distance: "2345km away",
+        availability: "available for 2 weeks stay",
+        rating: StarRating,
+
+    },
+    {
+		img: LocationImg2,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg3,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg4,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg5,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg6,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg7,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg8,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg9,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg11,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg12,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg13,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg14,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg15,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg16,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+    {
+		img: LocationImg17,
+		name: "Desert king",
+		price: "1MBT per night",
+		distance: "2345km away",
+		availability: "available for 2 weeks stay",
+		rating: StarRating,
+	},
+
+   
+];
+
+export const PlaceToStayFilters = [
+	{
+		name: "Restaurant",
+		link: "/",
+	},
+	{
+		name: "Cottage",
+		link: "/",
+	},
+	{
+		name: "Castle",
+		link: "/",
+	},
+	{
+		name: "Fantast City",
+		link: "/",
+	},
+	{
+		name: "Beach",
+		link: "/",
+	},
+	{
+		name: "Carbins",
+		link: "/",
+	},
+	{
+		name: "Off-grid",
+		link: "/",
+	},
+	{
+		name: "Farm",
+		link: "/",
+	},
+];
